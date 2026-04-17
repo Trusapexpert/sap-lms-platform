5aebe6213bc4e52bb54a78e16ea1b99aa6c8b709
Trusapexpert <trusapexpert@gmail.com>
2026-04-18 02:56:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,31 @@
-const express=require("express");
-const app=express();
-app.use(express.json());
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
-app.post("/progress",(req,res)=>{
-res.send("saved");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+host:"localhost",
+user:"root",
+password:"",
+database:"sap_lms"
+});
+
+app.post("/login",(req,res)=>{
+const {username,password}=req.body;
+
+db.query(
+"SELECT * FROM users WHERE username=? AND password=?",
+[username,password],
+(err,result)=>{
+if(result.length>0){
+res.json({success:true,role:result[0].role})
+}else{
+res.json({success:false})
+}
+});
 });
 
 app.listen(3000);
