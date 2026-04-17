0fda562ceac666bbe32d52609773b25e613b9e1f
Trusapexpert <trusapexpert@gmail.com>
2026-04-18 02:43:26 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-
+function markComplete(){
+fetch("http://localhost:3000/progress",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({user_id:1,course:"SAP",lesson:"Intro",completed:1})
+});
+}
